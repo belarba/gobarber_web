8c28ef83c289d8293b2ea8fbc1717b7baac86cf7
Daniel Belarmino <belarba@gmail.com>
2020-06-10 18:16:01 -0900

    first step

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
-  <h1>Olá mundo</h1>
+  <>
+    <h1>Olá mundo</h1>
+    <GlobalStyle />
+  </>
 );
 
 export default App;
